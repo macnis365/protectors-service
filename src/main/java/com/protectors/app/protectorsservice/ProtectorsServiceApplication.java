9696502b328e2aa62e31cf754f97198faff57bc0
micheal <macnis365@gmail.com>
2019-06-22 20:26:46 +0530

    inital springboot setup

---
+++ src/main/java/com/protectors/app/protectorsservice/ProtectorsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.protectors.app.protectorsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtectorsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtectorsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/protectors/app/protectorsservice/ProtectorsServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.protectors.app.protectorsservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProtectorsServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
