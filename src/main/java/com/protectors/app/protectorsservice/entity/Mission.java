5dc24c91389700391dc39d051902575e9b3a97b5
macnis365 <macnis365@gmail.com>
2019-06-24 15:25:31 +0530

    Mission entity added

---
+++ src/main/java/com/protectors/app/protectorsservice/entity/Mission.java
@@ -0,0 +1,36 @@
+package com.protectors.app.protectorsservice.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "mission")
+public class Mission implements Serializable {
+    @Id
+    @Getter
+    @Setter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Getter
+    @Setter
+    private String name;
+
+    @Getter
+    @Setter
+    private boolean isCompleted;
+
+    @Getter
+    @Setter
+    private boolean isDeleted;
+
+    @OneToMany(mappedBy = "missions", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JsonBackReference
+    private Set<Superhero> superheroes = new HashSet<>();
+}
--- src/main/java/com/protectors/app/protectorsservice/entity/Superhero.java
+++ src/main/java/com/protectors/app/protectorsservice/entity/Superhero.java
@@ -1,13 +1,17 @@
 package com.protectors.app.protectorsservice.entity;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
-@Table(name = "Superhero")
-public class Superhero {
+@Table(name = "superhero")
+public class Superhero implements Serializable {
 
     @Id
     @Getter
@@ -17,17 +21,30 @@ public class Superhero {
 
     @Getter
     @Setter
+/*
     @Column(name = "FirstName")
+*/
     private String firstName;
 
     @Getter
     @Setter
+/*
     @Column(name = "LastName")
+*/
     private String lastName;
 
     @Getter
     @Setter
+/*
     @Column(name = "SuperheroName")
+*/
     private String superheroName;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "superhero_missions",
+            joinColumns = {@JoinColumn(name = "superhero_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "mission_id", referencedColumnName = "id")})
+    @JsonManagedReference
+    private Set<Mission> missions = new HashSet<>();
 
 }
