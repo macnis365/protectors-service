0a4d915b990a63b489703e3009f59b8b96230d66
macnis365 <macnis365@gmail.com>
2019-06-25 21:07:21 +0530

    CompletedMissionCannotDelete exception

--- src/main/java/com/protectors/app/protectorsservice/customexception/CompletedMissionCannotDelete.java
+++ src/main/java/com/protectors/app/protectorsservice/customexception/CompletedMissionCannotDelete.java
@@ -1,6 +1,16 @@
 package com.protectors.app.protectorsservice.customexception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class CompletedMissionCannotDelete extends RuntimeException {
     public CompletedMissionCannotDelete(Long id) {
+        super("Completed mission cannot be deleted");
+    }
+
+    @Override
+    public synchronized Throwable fillInStackTrace() {
+        return this.getCause();
     }
 }
